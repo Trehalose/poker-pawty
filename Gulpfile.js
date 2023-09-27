55e9b07657febb11d47798fe6ba4941aaa6391ab
Trehalose <Trehalose@users.noreply.github.com>
2023-09-27 05:49:47 -0400

    RENAMED css file, ADDED more

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,11 @@ const merge = require('merge2');
 gulp.task('minify-css', () => {
      // Folder with files to minify
 	return merge(
-		gulp.src('./src/styles/index.css')
+		gulp.src('./src/styles/foundation.css'),
+		gulp.src('./src/styles/animations.css'),
+		gulp.src('./src/styles/ui.css'),
+		gulp.src('./src/styles/hand.css'),
+		gulp.src('./src/styles/players.css')
 	)
 		.pipe(cleanCSS())
 		.pipe(concat('index.css'))
